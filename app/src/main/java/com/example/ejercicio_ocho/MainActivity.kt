c2ab444cb732d3ac3ab4c2455eff38108a24651e
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2024-03-19 08:46:31 -0600

    Add windows

--- app/src/main/java/com/example/ejercicio_ocho/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio_ocho/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.ejercicio_ocho
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rcv: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        rcv = findViewById(R.id.rvContact)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("Contact", "Hay ${ProvicionalData.listContact.size} register contact")
+        rcv.adapter = Adapter()
+        rcv.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun btnAdd(v: View) {
+        val intent = Intent(this, AddActivity::class.java)
+        startActivity(intent)
     }
 }
